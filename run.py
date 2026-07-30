44dd710a1c99bcc794fd5c44ef49c278846db578
Danny Frazier <mementothis@outlook.com>
2026-07-29 22:30:10 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-# This line fixes the folder path issue automatically
-sys.path.append(os.path.join(os.path.dirname(__file__), 'satellite_game'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'orbital_command'))
 
-from satellite_game import main
+from orbital_command import main
 
 if __name__ == "__main__":
     main.play_orbital_command()
